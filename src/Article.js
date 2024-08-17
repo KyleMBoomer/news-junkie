d61e265fb0381d736a921c547dc6f897dd71e218
Kyle Mark Boomer <kylemarkboomer@Kyles-MacBook-Pro.local>
2024-08-17 12:15:01 -0500

    Initialize Article Styling

--- src/Article.js
+++ src/Article.js
@@ -4,6 +4,7 @@ import { getArticles } from '../apicalls'
 import Glide from '@glidejs/glide'
 import '@glidejs/glide/dist/css/glide.core.min.css'
 import '@glidejs/glide/dist/css/glide.theme.min.css'
+import './Article.css'
 
 const Article = () => {
     const [articles, setArticles] = useState([])
